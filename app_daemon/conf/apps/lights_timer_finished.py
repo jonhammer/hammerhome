270237a05b62841b813ce7017588493cc558da3d
Jon <jon.p.hammer@gmail.com>
2023-01-23 12:19:50 -0500

    Don't turn off lights at end of time window

--- app_daemon/conf/apps/lights_timer_finished.py
+++ app_daemon/conf/apps/lights_timer_finished.py
@@ -4,16 +4,13 @@ import re
 # pylint: disable=import-error
 import hassapi as hass
 import yaml
+import HammerHass
 
-ROOM_RE = r"timer\.(.*)_light_timer"
 HOME_DATA_FILE = os.environ.get("HOME_DATA_FILE") or "/conf/apps/home.yaml"
 DEBUG = os.environ.get("DEBUG")
 
 
-class LightsTimerFinished(hass.Hass):
-
-    with open(HOME_DATA_FILE, "r") as f:
-        HOME_DATA = yaml.load(f, Loader=yaml.Loader)
+class LightsTimerFinished(HammerHass.HammerHass):
 
     def initialize(self):
         self.log("---------------------------------------")
@@ -23,7 +20,17 @@ class LightsTimerFinished(hass.Hass):
 
     def timer_finished_callback(self, _event_name, data, _kwargs=None):
         """Appdaemon callback for lights timer finished logic"""
-        room_name = re.search(ROOM_RE, data["entity_id"]).group(1)
+        room_name = self.get_room_name(data["entity_id"])
+
+        if self.is_in_override(room_name):
+            self.log(f"{room_name} lights are in override state, will not turn off lights.")
+            return
+
+        light_data = self.get_light_data(room_name)
+        if not self.within_required_time(light_data):
+            self.log(f"{room_name} lights are not within required time, will not turn off lights.")
+            return
+
         lights_name = f"light.{room_name}_lights"
         self.log(f"Turning off: {lights_name}")
         try:
