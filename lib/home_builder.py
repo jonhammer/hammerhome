c194c2034f6a8c930dae1089ae9e5ee9f9a85ed7
Jon <jon.p.hammer@gmail.com>
2023-01-26 13:51:58 -0500

    Add YAML linter

--- lib/home_builder.py
+++ lib/home_builder.py
@@ -16,3 +16,9 @@ with open(HOME_CFG_PATH, "r") as f:
     home_data = yaml.load(f, Loader=yaml.FullLoader)
 home = Home.from_dict(home_data)
 
+prefix = 'binary_sensor'
+postfix = 'motion_sensor_0_occupancy'
+for area in home.areas:
+    print(f"  - {prefix}.{area.name}_{postfix}")
+    for room in area.rooms:
+        print(f"  - {prefix}.{room.name}_{postfix}")
